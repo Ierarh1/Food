0663f05a7eb3881e0cedfcdd5690d157627b49ea
Andrey Ilc <geleon2170@gmail.com>
2023-03-08 21:03:52 +0300

    triggered MODAL. Added other function showModalByScroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
-    //TABS
+
+
+
+
+                                            //TABS
+
+
     const tabsParent  = document.querySelector('.tabheader__items');
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
@@ -69,83 +75,229 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 
-    //TIMER
+                                                         //TIMER
 
-    const deadLine = '2020-3-10';
+//почему время в формате такой строки??
+//всё просто в будущем скорее всего прийдётся подвязывать наш сайт под какую нибудь административную панель
+//а такая панель чаще всего возвращает время в формате string
+const deadLine  = '2025-02-23';                            
 
 
-    function getTimeRemaining(endTime)
+
+//функция которая определяет разницу между deadLine  и нашим текущим временем
+function getTimeRemaining(endtime){
+	//превращаем нашу строку в число
+	//Date.parse(endtime) - это конечное время в миллисекундах
+	//Date.parse(new Date()) - это наше нынешнее время да ещё и в миллисекундах
+	const t = Date.parse(endtime) - Date.parse(new Date());
+
+
+	//теперь надо полученную разницу t -превратить в количество ДНЕЙ ЧАСОВ МИНУТ СЕКУНД(это я из макета взял там такое требование)
+	//получим количество дней 
+	//Math.floor - округляет до ближайшего целого
+	const days = Math.floor(t/(1000*60*60*24));
+
+	//дальше часы минуты.... там чуть сложнее но разберёшься
+	const hours = Math.floor(((t/1000*60*60)%24));
+	const minutes=Math.floor(((t/1000*60)%60));
+	const seconds=Math.floor(((t/1000)%60));
+
+	//теперь нам надо вернуть эти данные наружу. Вёрнём их ОБЪЕКТОМ
+	return{
+		'total':t,
+		'days': days,
+		'hours':hours,
+		'minutes':minutes,
+		'seconds':seconds,
+	};
+}
+
+///эх, короче. мы хотим что время в наш таймер подставлялось в формате 00. Т. е. если у нас 
+//в таймер попало 9 часов, мы хотим что бы оно автоматом превращалось в формат 09
+function getZero(num){
+	//проверяем на барахло num>=0
+	//прибавлять ноль нужно только числам от 0 до 9, поэтому условие num<10
+	if(num>=0 && num<10)
+	{
+		return `0${num}`;
+	}
+	else{
+		return num;
+	}
+}
+
+
+//теперь непосредственно та функция которая будет устанавливать время на нашу страничку
+//1)аргумент это селектор где находятся все наши блоки со временем
+//2)аргумент, нам понадобится что бы тормознуть таймер,  иначе хрен пойми,время уже вышло, а тамер будет хреначить
+function setClock(selector, endtime){
+	const timer = document.querySelector(selector),
+		  days = document.querySelector('#days'),
+		  hours = document.querySelector('#hours'),
+		  minutes = document.querySelector('#minutes'),
+		  seconds = document.querySelector('#seconds');
+
+	//так же нам нужно что-то будет запускать нашу нижнюю функцию дабы КАЖДУЮ секунду обновлять таймер
+	const timeInterval = setInterval(updateClock,1000);
+
+	//когда запускается setInterval(updateClock,1000), то он вначале подождёт 1 секунду а потом стартанёт
+	//на вёрстке это будет выглядеть как баг МЕРЦАНИЯ времени(т.к по умолчанию в вёрстке стоят свои значения)
+	//и фиксится этот баг тем что мы один раз мы вызовём функцию сами БЕЗ ТАЙМЕРА
+	updateClock();
+
+	//теперь нам нужна функция которая будет обновлять таймер каждую секунду
+	function updateClock(){
+		//расчёт времени которое нам осталось ПРЯМ НА ЭТУ СЕКУНДУ
+		const t = getTimeRemaining(endtime);//вёрнёт своим результатом объект набитый данными days/hours......
+
+		//теперь непосредственно вносим на нашу страницу
+		days.innerHTML = getZero(t.days);
+		hours.innerHTML = getZero(t.hours);
+		minutes.innerHTML = getZero(t.minutes);
+		seconds.innerHTML = getZero(t.seconds);
+
+		//мы выше сделали автообновление функци каждую секунду. и поэтому нам необходимо будет
+		//СТОПАРНУТЬ нашу функцию updateClock() как только таймер истечёт для этого мы и брали переменную total
+		if(t.total<=0){
+			clearInterval(timeInterval);
+		}
+	 }
+}
+
+//заводим нашу функцию
+setClock('.timer',deadLine);
+
+
+
+
+                                                    //MODAL
+//кнопки вызоват модальных окон с дата атрибутами data-modal
+const modalTrigger = document.querySelectorAll('[data-modal]');
+//само модальное окно
+const modal        = document.querySelector('.modal');
+//кнопка закрытия модального окна
+const modalCloseBtn= document.querySelector('[data-close]');
+
+
+//логика такая. Нам надо всего лишь две функции 1)открывает модалку. 2)закрывает модалку
+
+//открываем модальное окно
+modalTrigger.forEach(item=>{
+    item.addEventListener('click',openModal);
+});
+
+
+function openModal()
+{
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+
+    //замораживаем прокрутку
+    document.body.style.overflow = 'hidden';
+
+    //очищаем интервал открытия окна. Если пользователь сам открыл окно то мы больше ему его показывать не будем
+    clearInterval(modalTimer);
+}
+
+
+//функция по закрытию окна
+function closeModal()
+{
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+    //восстанавливаем работу прокрутки
+    document.body.style.overflow = '';
+}
+
+
+//закрываем модальное окно
+modalCloseBtn.addEventListener('click',closeModal);
+
+
+//как сделать так что бы пользователь кликал на подложку модального окна и оно закрывалось
+//важно что бы html была нормальная структура что бы modal__dialog была рабочей областью
+//а уже Modal была полдожкой.
+modal.addEventListener('click',(event)=>{
+    if(modal === event.target)
     {
-        let days, hours, minutes, seconds;
-        const t = Date.parse(endTime) - Date.parse(new Date());
-
-
-        if(t<=0)
-        {
-            days = hours = minutes = seconds = 0;
-        }else{
-            days = Math.floor(t / (1000*60*60*24)),
-            hours =  Math.floor((t / (1000*60*60*24)) % 24),
-            minutes = Math.floor(t / (1000*60) % 60),
-            seconds = Math.floor((t/1000)%60);
-        }
-
-        
-        return{
-            'total': t,
-            'days':  days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds,
-        };
-        
-
+        closeModal();
     }
+});
 
-    function getZero(num)
+
+
+//закрываем модальное окно с помощью кнопки ESC
+document.addEventListener('keydown',(e)=>{
+
+    //если нажатая кнопка это 'Escape'.
+    //а второе условие contains('show') - это проверяем что модальное окно ОТКРЫТО
+    if(e.code ==='Escape' && modal.classList.contains('show'))
     {
-        if(num<10)
-        {
-            return `0${num}`
-        }
-        else{
-            return num;
-        }
-
+        closeModal();
     }
-
-
-    function setClock(selector, endtime)
-    {
-        const timer = document.querySelector(selector);
-        const days = timer.querySelector('#days');
-        const hours = timer.querySelector('#hours');
-        const minutes = timer.querySelector('#minutes');
-        const seconds = timer.querySelector('#seconds');
-
-        const timeInterval = setInterval(updateClock,1000);
-        updateClock();
-
-        function updateClock()
-        {
-            const t = getTimeRemaining(endtime);
-            days.innerHTML = getZero(t['days']);
-            hours.innerHTML = getZero(t['hours']);
-            minutes.innerHTML = getZero(t['minutes']);
-            seconds.innerHTML = getZero(t['seconds']); 
-            if(t['total']<=0)
-            {
-                clearInterval(timeInterval);
-            }
-        }
-
-        
-    }
-
-
-
-    setClock('.timer', deadLine);
 
 
 });
 
+
+//Через какое -то время будет появляться модальное окно.
+const modalTimer = setTimeout(openModal, 3000);
+
+
+
+//реализуем фичу, когда модальное окно открывается когда пользователь доскроллил доконца
+function showModalByScroll(){
+    // window.pageXOffset -ПРОКРУЧЕННАЯ ЧАСТЬ
+    //document.documentElement.clientHeight -видимая часть  которую мы видим без прокрутки
+    if(window.pageXOffset + document.documentElement.clientHeight >= 
+        document.documentElement.scrollHeight-1)
+    {
+        openModal();
+        window.removeEventListener('scroll',showModalByScroll);
+    }
+}
+
+
+
+
+
+window.addEventListener('scroll',showModalByScroll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
