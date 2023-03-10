9bb22b1a4240357a6367a1a16bd52e18bf3de456
Andrey Ilc <geleon2170@gmail.com>
2023-03-10 20:38:11 +0300

    Deleted food-cart in index.html and make their in script.js with class

--- js/script.js
+++ js/script.js
@@ -242,7 +242,18 @@ document.addEventListener('keydown',(e)=>{
 
 
 //Через какое -то время будет появляться модальное окно.
-const modalTimer = setTimeout(openModal, 3000);
+
+
+
+
+
+
+
+
+
+                                            //расскоментируй
+//const modalTimer = setTimeout(openModal, 3000);
+
 
 
 
@@ -268,11 +279,83 @@ window.addEventListener('scroll',showModalByScroll);
 
 
 
+                                                    //Carts(with class)
+class MenuCart{
+    constructor(src,alt,title,desr,price,parentSelector){
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.desr = desr;
+        this.price= price;
+        //родительский элемент КУДА будем вставлять наш класс
+        this.parent = document.querySelector(parentSelector);
+        //курс валюты
+        this.transfer = 27;
+        //кстати в конструкторе позволительно вызывать методы класса
+        this.price = this.changeToUAH();
+    }
+    changeToUAH(){
+       return (this.price = this.price * this.transfer);
+    }
 
 
+    render()
+    {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.desr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
+
+        //помещаем наш элемент на страницу
+        this.parent.append(element);
+    }
+}
 
 
+//сокращённый синтаксис вызова нашего класса, без создания переменной
+//и на этом же классе мы вызываем метод 
+// new MenuCart().render();
+//
+//однако нам надо передать много аргументов поэтому мы его разорвём. Первый пример был для наглядности
+new MenuCart(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
 
+).render();
+
+new MenuCart(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан',
+    5,
+    '.menu .container'
+
+).render();
+
+
+new MenuCart(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+    10,
+    '.menu .container'
+
+).render();
 
 
 
--- js/script1.js
+++ js/script1.js
@@ -1,117 +1,32 @@
-const tabsContent = document.querySelectorAll('.tabcontent');
-const tabs       = document.querySelectorAll('.tabheader__item');
-const tabsParent= document.querySelector('.tabheader__items');
-
-
-
-
-
-function hideTabsContent()
-{
-    tabsContent.forEach((item)=>{
-        item.classList.add('hide');
-        item.classList.remove('show','fade');
-    });
-
-    function deleteClassActive()
+class Test{
+    constructor(test1,test2)
     {
-        tabs.forEach((item)=>{
-            item.classList.remove('tabheader__item_active');
-        });
+        this.test1 = test1;
+        this.test2 = test2;
     }
 
-    deleteClassActive();
-}
-
-
-function showTabContent(i=0)
-{
-    tabsContent[i].classList.add('show','fade');
-    tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add('tabheader__item_active');
-}
-
-tabsParent.addEventListener('click',(event)=>{
-    const target = event.target;
-
-    tabs.forEach((item,i)=>{
-        if(target==item && item.classList.contains('tabheader__item'))
-        {
-            hideTabsContent();
-            showTabContent(i);
-        }
-    });
-
-});
-
-
-hideTabsContent();
-showTabContent();
-
-
-
-
-
-
-
-//timer
-const deadLine = '2023-3-8';
-
-function calculatingTime(endtime)
-{
-    const remainingTimeMS = Date.parse(endtime) - Date.parse(new Date()); 
-
-    const days    = Math.floor(remainingTimeMS/(1000*60*60*24));
-    const hours   = Math.floor(remainingTimeMS/(1000*60*60)%24);
-    const minutes = Math.floor(remainingTimeMS/(1000*60)%60);
-    const seconds = Math.floor(remainingTimeMS/(1000)%60);
-
-    return {
-        'total'   : remainingTimeMS,
-        'days'    : days,
-        'hours'   : hours,
-        'minutes' : minutes,
-        'seconds' : seconds,
-    }
-}
-
-function getZero(num){
-    if(num<10)
+    createMultiple()
     {
-        return `0${num}`
-    }
-    else
-    {
-        return num;
+        return this.test1 * this.test2;
     }
 }
 
-function setClock(selector, endtime)
-{
-    const timer    = document.querySelector(selector);
-    const days     = timer.querySelector('#days');
-    const hours    = timer.querySelector('#hours');
-    const minutes  = timer.querySelector('#minutes');
-    const seconds  = timer.querySelector('#seconds');
-
-    const interval = setInterval(updateClock,1000);
+class tester extends Test{
+    constructor(test1,test2, text, number)
+    {
+        super(test1,test2);
+        this.text = text;
+        this.number = number;
+    }
     
-    updateClock();
-
-    function updateClock()
+    showTest()
     {
-        const time = calculatingTime(endtime);
-
-        days.innerHTML = getZero(time.days);
-        hours.innerHTML= getZero(time.days);
-        minutes.innerHTML= getZero(time.minutes);
-        seconds.innerHTML= getZero(time.seconds);
-
-        if(time.total<=0)
-        {
-            clearInterval(interval);
-        }
+        console.log(`text = ${this.text}, number = ${this.number}`);
     }
 }
 
-setClock('.timer', deadLine);
+const newElement = new tester(700,900,`lorem`, '200');
+
+console.log(newElement.createMultiple());
+
+newElement.showTest();
